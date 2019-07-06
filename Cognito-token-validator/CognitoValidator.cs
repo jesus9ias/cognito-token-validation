29f04c0f0315c97d5e428f7182fbbfbff490744a
Jesús Escamilla <jesusescamilla@hotmail.com>
2019-07-06 15:37:58 -0500

    start proyect

---
+++ Cognito-token-validator/CognitoValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cognito_token_validator
+{
+    public class CognitoValidator
+    {
+        public string ValidateToken()
+        {
+            return "ok";
+        }
+    }
+}
